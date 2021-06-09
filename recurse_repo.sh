e933864fffd01e82c7ddab7e84587b4998b29ef9
hewenjie <1021458687@qq.com>
2021-06-09 23:28:17 +0800

    add BUILD yaml

--- recurse_repo.sh
+++ recurse_repo.sh
@@ -14,7 +14,7 @@ while [[ ${#recurse_dir_list[*]} -gt 0 ]];do
         # python 读取repo 中的yaml文件的依赖项
         # 打印出依赖的库目录和依赖分支
         # 脚本根据支去输出依赖文件
-        depend_result=`python ./bin/get_depend_list.py $value`
+        depend_result=`python ./get_depend_list.py $value`
         for d in "${depend_result[@]}"
         do
             # d格式为repo + branch，示例:lianjin/libtest master
@@ -22,7 +22,7 @@ while [[ ${#recurse_dir_list[*]} -gt 0 ]];do
             repo_url=`echo $d | awk -F " " '{print $1}'`
             branch=`echo $d | awk -F " " '{print $2}'`
             local_dir=`echo $repo_url | awk -F ':' '{print $2}' | awk -F ".git" '{print $1}'`
-            local_dir="/codebase/"$local_dir
+            local_dir="~/codebase/"$local_dir
             # 判断本地是否存在repo和branch，不存在则创建
             if [ ! -d $local_dir ];then
                 git clone -b $branch $repo_url $local_dir
